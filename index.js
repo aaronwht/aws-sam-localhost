d21a71c618d3c696891eb540e867b6a6b67a36ff
Aaron White <awhite@tarvold.com>
2019-10-15 18:44:56 -0700

    Added index and template files

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+// sam local start-api
+module.exports.handler = async (event) => {  
+    const { body } = event
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: body
+    }
+}
